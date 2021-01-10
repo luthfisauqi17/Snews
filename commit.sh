1ef67e93f939748a973f311cfcb9c24faf884724
luthfisauqi17 <sauqiluthfi877@gmail.com>
2021-01-10 12:29:04 +0700

    minor changes in commit.sh

--- commit.sh
+++ commit.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
-
-# blog
-# tambahkan perubahan ke Git
 git add -A
-# Buat sebuah commit baru
 msg="Project commit at `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
 git commit -m "$msg"
-# Push atau puload ke Github
 git push origin main
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,7 @@
+import { Carousel } from "bootstrap";
 import React from "react";
 import { Jumbotron, Button, Figure } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { Helmet } from "react-helmet";
 
 function Home(props) {
@@ -15,15 +17,45 @@ function Home(props) {
         <h1 className="text-center">
           Welcome to <strong>SNEWS</strong>
         </h1>
-        <Figure>
-          <Figure.Image
-            width={1500}
-            height={785}
-            alt={random_image}
-            src={random_image}
-          />
-          <Figure.Caption>Source: {random_image}</Figure.Caption>
-        </Figure>
+        <Carousel>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="holder.js/800x400?text=First slide&bg=373940"
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="holder.js/800x400?text=Second slide&bg=282c34"
+              alt="Third slide"
+            />
+
+            <Carousel.Caption>
+              <h3>Second slide label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="holder.js/800x400?text=Third slide&bg=20232a"
+              alt="Third slide"
+            />
+
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
         <Button
           className="btn btn-primary btn-lg btn-block"
           href="/news"
@@ -37,3 +69,15 @@ function Home(props) {
 }
 
 export default Home;
+
+{
+  /* <Figure>
+  <Figure.Image
+    width={1500}
+    height={785}
+    alt={random_image}
+    src={random_image}
+  />
+  <Figure.Caption>Source: {random_image}</Figure.Caption>
+</Figure> */
+}
