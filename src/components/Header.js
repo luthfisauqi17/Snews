43c02ac2ded6b17a0fd0887843084a7e4368d806
luthfisauqi17 <sauqiluthfi877@gmail.com>
2021-01-10 13:01:10 +0700

    Add sticky navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,28 +4,32 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function Header(props) {
   return (
-    <div className="mb-4">
-      <Navbar bg="dark" variant="dark" expand="lg">
-        <Navbar.Brand href="/home">SNEWS</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto">
-            <Nav.Link
-              className="btn btn-primary ml-1 mr-1 mt-1 mb-1 active"
-              href="/home"
-            >
-              Home
-            </Nav.Link>
-            <Nav.Link
-              className="btn btn-secondary ml-1 mr-1 mt-1 mb-1"
-              href="/news"
-            >
-              News
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    </div>
+    <Navbar
+      className="mb-4"
+      bg="light"
+      variant="light"
+      expand="lg"
+      sticky={"top"}
+    >
+      <Navbar.Brand href="/home">SNEWS</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Link
+            className="text-primary btn btn-outline-primary ml-1 mr-1 mt-1 mb-1"
+            href="/home"
+          >
+            Home
+          </Nav.Link>
+          <Nav.Link
+            className="text-info btn btn-outline-info ml-1 mr-1 mt-1 mb-1"
+            href="/news"
+          >
+            News
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,18 @@
-import { Carousel } from "bootstrap";
 import React from "react";
-import { Jumbotron, Button, Figure } from "react-bootstrap";
+import { Jumbotron, Button, Carousel, Figure } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Helmet } from "react-helmet";
 
 function Home(props) {
-  const random_image = `https://picsum.photos/seed/${Math.floor(
+  const random_image_1 = `https://picsum.photos/seed/${Math.floor(
+    Math.random() * 1001
+  )}/1500/785`;
+
+  const random_image_2 = `https://picsum.photos/seed/${Math.floor(
+    Math.random() * 1001
+  )}/1500/785`;
+
+  const random_image_3 = `https://picsum.photos/seed/${Math.floor(
     Math.random() * 1001
   )}/1500/785`;
   return (
@@ -17,43 +24,39 @@ function Home(props) {
         <h1 className="text-center">
           Welcome to <strong>SNEWS</strong>
         </h1>
-        <Carousel>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="holder.js/800x400?text=First slide&bg=373940"
-              alt="First slide"
-            />
-            <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
+        <Carousel indicators={false}>
+          <Carousel.Item interval={3000}>
+            <Figure>
+              <Figure.Image
+                width={1500}
+                height={785}
+                alt={random_image_1}
+                src={random_image_1}
+              />
+              <Figure.Caption>Source: {random_image_1}</Figure.Caption>
+            </Figure>
           </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="holder.js/800x400?text=Second slide&bg=282c34"
-              alt="Third slide"
-            />
-
-            <Carousel.Caption>
-              <h3>Second slide label</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </Carousel.Caption>
+          <Carousel.Item interval={3000}>
+            <Figure>
+              <Figure.Image
+                width={1500}
+                height={785}
+                alt={random_image_2}
+                src={random_image_2}
+              />
+              <Figure.Caption>Source: {random_image_2}</Figure.Caption>
+            </Figure>
           </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="holder.js/800x400?text=Third slide&bg=20232a"
-              alt="Third slide"
-            />
-
-            <Carousel.Caption>
-              <h3>Third slide label</h3>
-              <p>
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-              </p>
-            </Carousel.Caption>
+          <Carousel.Item interval={3000}>
+            <Figure>
+              <Figure.Image
+                width={1500}
+                height={785}
+                alt={random_image_3}
+                src={random_image_3}
+              />
+              <Figure.Caption>Source: {random_image_3}</Figure.Caption>
+            </Figure>
           </Carousel.Item>
         </Carousel>
         <Button
@@ -69,15 +72,3 @@ function Home(props) {
 }
 
 export default Home;
-
-{
-  /* <Figure>
-  <Figure.Image
-    width={1500}
-    height={785}
-    alt={random_image}
-    src={random_image}
-  />
-  <Figure.Caption>Source: {random_image}</Figure.Caption>
-</Figure> */
-}
